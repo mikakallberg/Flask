6c638480f143a7f468f00de97241446cb949af5a
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-02 14:15:22 +0000

    Line 3-5 ways to insert html

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,7 +18,10 @@ def about():
     """
     Build path to About page
     """
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
@@ -25,12 +29,12 @@ def contact():
     """
     Build path to Contact page
     """
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
